e3f0f5d903189af6763eba89cbe8055e787f0d04
Chaudhary Shaharyar Tariq <shaharyar.tariq248@gmail.com>
2022-04-02 23:10:46 +0500

    env changes

--- backend/passport.js
+++ backend/passport.js
@@ -2,11 +2,11 @@
 var GoogleStrategy = require("passport-google-oauth20").Strategy;
 var GitHubStrategy = require("passport-github").Strategy;
 const passport = require("passport");
-const GOOGLE_CLIENT_ID =
-  "647812996023-k3dgq0h4jrlq2rbsg2ajj66m5ebejqqt.apps.googleusercontent.com";
-const GOOGLE_CLIENT_SECRET = "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";
-const GITHUB_CLIENT_ID = "b0dde886dedfc91064d3";
-const GITHUB_CLIENT_SECRET = "ae1e31d9d784284422012cb8fb54a1705b0b2ce8";
+// const GOOGLE_CLIENT_ID =
+//   "647812996023-k3dgq0h4jrlq2rbsg2ajj66m5ebejqqt.apps.googleusercontent.com";
+// const GOOGLE_CLIENT_SECRET = "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";
+// const GITHUB_CLIENT_ID = "b0dde886dedfc91064d3";
+// const GITHUB_CLIENT_SECRET = "ae1e31d9d784284422012cb8fb54a1705b0b2ce8";
 
 passport.use(
   new GoogleStrategy(
